2b4856f8c1f4639cd2251920adf0c30d803e5923
Tyler Schrock <tschrock123@gmail.com>
2023-07-17 23:49:50 -0400

    Add cookie auth and better error messaging

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,25 @@ interface oEmbed {
   provider_name: string
 }
 
+export interface Env {
+  DA_COOKIE: string;
+}
+
 export default {
-  async fetch(request: Request): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url)
     url.hostname = url.hostname.replace("fixdeviantart.com", "deviantart.com")
 
     const embedUrl = new URL('https://backend.deviantart.com/oembed')
     embedUrl.searchParams.set('url', url.toString())
 
-    const embedResponse = await fetch(embedUrl.toString())
+    const cookies: Record<string, string> = JSON.parse(env.DA_COOKIE)
+
+    const embedResponse = await fetch(embedUrl.toString(), {
+      headers: {
+        'Cookie': Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ')
+      }
+    })
 
     if (embedResponse.ok) {
       const data = await embedResponse.json<oEmbed>()
@@ -32,7 +42,7 @@ export default {
   <meta property="og:title" content="${escape(data.title)}">
   <meta property="og:site_name" content="${escape(data.provider_name)}">
   <meta property="og:image" content="${escape(data.url)}">
-
+  
   <!-- Twitter Card -->
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="${escape(data.title)}" />
@@ -54,11 +64,37 @@ export default {
         }
       })
     } else {
-      return new Response(null, {
-        status: 301,
-        statusText: 'Moved Permanently',
+      let title = "Oops, something went wrong"
+      let description = `Yell at @CyberPon3 to fix this\nStatus: ${embedResponse.status} ${embedResponse.statusText}\nResponse: ${await embedResponse.text()}`
+      console.error(new Error(`Failed to fetch ${embedUrl.toString()}: ${description}`))
+      return new Response(`<!DOCTYPE html>
+<html>
+<head>
+
+  <!-- OpenGraph -->
+  <meta property="og:title" content="${escape(title)}">
+  <meta property="og:description" content="${escape(description)}">
+  <meta property="og:site_name" content="fixdeviantart.com">
+
+  <!-- Twitter Card -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="Oops, something went wrong" />
+  <meta name="twitter:description" content="${escape(description)}" />
+  <meta name="twitter:creator" content="@CyberPon3" />
+
+  <!-- Redirect real clients -->
+  <meta http-equiv="refresh" content="0; url=${escape(url.toString())}" />
+
+</head>
+<body>
+  Redirecting...<br>
+  <a href="${escape(url.toString())}">Click here if your browser broke.</a>
+</body>
+</html>`, {
+        status: 200,
+        statusText: 'OK',
         headers: {
-          'Location': url.toString()
+          'Content-Type': 'text/html; charset=UTF-8'
         }
       })
     }
